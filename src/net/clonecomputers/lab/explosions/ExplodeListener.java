16524cd3f6d65d534e65ee300f41bc1944631f05
g_rocket <g.rocket1@gmail.com>
2014-05-25 18:43:19 -0400

    remove unused imports and unnecessary boilerplate code

--- src/net/clonecomputers/lab/explosions/ExplodeListener.java
+++ src/net/clonecomputers/lab/explosions/ExplodeListener.java
@@ -4,12 +4,6 @@ import org.bukkit.event.*;
 import org.bukkit.event.entity.*;
 
 public class ExplodeListener implements Listener {
-	ExplosionDropAll plugin;
-
-	public ExplodeListener(ExplosionDropAll plugin){
-		this.plugin = plugin;
-	}
-
 	@EventHandler
 	public void handleExplosion(EntityExplodeEvent e){
 		e.setYield(1);
--- src/net/clonecomputers/lab/explosions/ExplosionDropAll.java
+++ src/net/clonecomputers/lab/explosions/ExplosionDropAll.java
@@ -1,14 +1,9 @@
 package net.clonecomputers.lab.explosions;
 
-import java.util.*;
-
 import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.*;
 
 public class ExplosionDropAll extends JavaPlugin {
-	
-	private final ExplodeListener explodeListener = new ExplodeListener(this);
-	
 	public void onDisable() {
 		getLogger().info("ExplosionDropAll 0.1 doesn't know how to disable itself");
 	}
@@ -16,6 +11,6 @@ public class ExplosionDropAll extends JavaPlugin {
 	public void onEnable() {
 		getLogger().info("ExplosionDropAll 0.1 Enabled");
         PluginManager pm = this.getServer().getPluginManager();
-        pm.registerEvents(explodeListener, this);
+        pm.registerEvents(new ExplodeListener(), this);
 	}
 }
