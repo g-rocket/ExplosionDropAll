8b2189500e62ee8dbea79e5919b3c4121b5205b9
g_rocket <g.rocket1@gmail.com>
2014-05-25 19:00:17 -0400

    plugin can now properly enable / disable itself

--- src/net/clonecomputers/lab/explosions/ExplosionDropAll.java
+++ src/net/clonecomputers/lab/explosions/ExplosionDropAll.java
@@ -1,16 +1,20 @@
 package net.clonecomputers.lab.explosions;
 
+import org.bukkit.event.entity.*;
 import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.*;
 
 public class ExplosionDropAll extends JavaPlugin {
+	
+	private final ExplodeListener explodeListener = new ExplodeListener();
+	
 	public void onDisable() {
-		getLogger().info("ExplosionDropAll 0.1 doesn't know how to disable itself");
+		getLogger().info("ExplosionDropAll 0.1 Disabled");
+		EntityExplodeEvent.getHandlerList().unregister(explodeListener);
 	}
 
 	public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(explodeListener, this);
 		getLogger().info("ExplosionDropAll 0.1 Enabled");
-        PluginManager pm = this.getServer().getPluginManager();
-        pm.registerEvents(new ExplodeListener(), this);
 	}
 }
